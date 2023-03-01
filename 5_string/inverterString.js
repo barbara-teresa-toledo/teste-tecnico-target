a2d5c60628f5b040e0cfa3a1aae0e890cfca759d
Barbara Teresa <barbara-teresa@outlook.com>
2023-03-01 17:22:42 -0300

    Teste 5 - Inverter string

---
+++ 5_string/inverterString.js
@@ -0,0 +1,12 @@
+function inverterString(stringOriginal) {
+  let stringInvertida = "";
+
+  for (let i = stringOriginal.length - 1; i >= 0; i--) {
+    stringInvertida += stringOriginal[i];
+  }
+
+  console.log(stringInvertida);
+}
+
+let stringParaInverter = "Hello, world!";
+inverterString(stringParaInverter);
